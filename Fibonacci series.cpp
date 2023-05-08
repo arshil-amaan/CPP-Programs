91d45eeb762ac9cd277a0a9d177f89eaeafc68b0
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-09 00:42:46 +0530

    Create Fibonacci series.cpp

---
+++ Fibonacci series.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+//fibonacci series number on place of 'num'
+int fibo(int n)
+{
+	if(n==1 || n==2)
+		return 1;
+	else
+	{	int a=1,b=1, c;
+	    for(int i=3;i<=n;i++)
+	    {
+	        c=a+b;
+	        a=b;
+	        b=c;
+	        //un-comment below line for full fibonacci series
+	        // cout<<c<<" ";
+	    }
+    	return c;
+    }
+}
+ 
+int main()
+{
+    int num;
+    cin>>num;
+    // cout<<"1 1 ";
+    cout<<fibo(num);
+}
