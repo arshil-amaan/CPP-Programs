3523e6dab3e70de1f2619e53e66fa96f9d164710
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-09 00:41:41 +0530

    Create digital root.cpp

---
+++ digital root.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+ 
+int sumOfAll(int a)
+{
+    int ld, sum=0;
+    while(a>0){
+        ld=a%10;
+        a=a/10;
+        sum=sum+ld;
+    }
+    return sum;
+}
+int main() {
+    int num;
+    cin>>num;
+	int t=sumOfAll(num);
+	while(t>9)
+	{
+	    t=sumOfAll(t);
+	}
+	// cout<<sumOfAll(t);
+	cout<<t;
+}
