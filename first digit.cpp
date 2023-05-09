f1b071469cab47bd3339425a2812c8340d797f9d
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-10 00:56:11 +0530

    Create first digit.cpp

---
+++ first digit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//First Digit of given Number
+int firstDigit(int num) {
+    int fd =abs(num);
+    //removes ones digit untill it is single digit
+    while(fd>9)
+    {
+        fd=fd/10;
+    }
+	return fd;
+}
+int main()
+{
+	int num;
+    cin>>num;
+    cout<<"first digit of "<<num<<" is "<<firstDigit(num);
+}
