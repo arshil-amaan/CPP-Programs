7a5022ece5cc90f8e32259ae40eabbf4ab9f951c
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-11 02:27:58 +0530

    Create passing methods.cpp

---
+++ passing methods.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// pass by value
+void fun(int x, int y){
+    cout<<"x: "<<x<<"\ny: "<<y<<endl;
+}
+
+// pass by reference
+void fun(int &x, int &y){
+    cout<<"x value : "<<x<<endl<<"y value : "<<y<<endl;
+}
+
+// pass by address
+void fun(int *x, int *y){
+    cout<<"x is : "<<*x<<endl<<"y is : "<<*y<<endl;
+}
+
+int main() {
+    int a=10,b=15;
+  // pass by value, pass by reference
+    fun(a,b);
+  // pass by address
+    fun(&a,&b);
+	return 0;
+}
