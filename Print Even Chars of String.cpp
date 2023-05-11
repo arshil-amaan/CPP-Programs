b7981c48f8ad87e9fcebf4b4dff32a729aef3160
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-12 03:01:20 +0530

    Create Print Even Chars of String.cpp

---
+++ Print Even Chars of String.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//access string characters by index
+int main() {
+    string sentence;
+    cin>>sentence;
+    //string_name.length() returns number of characters in string
+    for(int i= 0;i<=sentence.length();i=i+2)
+    //string_name[i] accesses ith character
+    cout<<sentence[i];
+	return 0;
+}
