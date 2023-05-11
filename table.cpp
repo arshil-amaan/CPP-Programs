67deb479e5f9a1bf7e7260cbefa50e31a6ff9e19
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-12 03:00:03 +0530

    Create Multiplication table.cpp

---
+++ table.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//table
+void table(int num)
+{
+    for(int i=1;i<=10;i++)
+        cout<<num*i<<" ";
+}
+int main() {
+    int num;
+    cin>>num;
+    table(num);
+	return 0;
+}
