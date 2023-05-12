a9576abc203fabd7a31c4455fca93784224f6b25
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-13 01:03:35 +0530

    Create digit counter.cpp

---
+++ digit counter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//how many digits in user provided integer
+int main()
+{
+    int num, digits=0;
+    cin>>num;
+    int n=num;
+    //Divide number by 10 and increase counter
+    while(n>0)
+    {
+        n=n/10;
+        digits++;
+    }
+    cout<<num<<" has "<<digits<<" Digits";
+	return 0;
+}
