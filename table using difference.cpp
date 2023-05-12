7ea3da7b12d025c61f1ebc7e504ca6cd3693a678
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-13 01:02:39 +0530

    Create table using difference.cpp

---
+++ table using difference.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//table by table differences
+int main()
+{
+    int n1, n2;
+    cin>>n1>>n2;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<(n1*i)-(n2*i)<<" ";
+    }
+	return 0;
+}
