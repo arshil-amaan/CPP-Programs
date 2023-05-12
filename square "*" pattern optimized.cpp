a920a80a8c495a5218203159472903bbde5a823a
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-13 01:05:04 +0530

    Create square "*" pattern optimized.cpp

---
+++ square "*" pattern optimized.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//how many digits in user provided integer
+int main() {
+    int s;
+    cin>>s;
+    for(int i=0;i<s;i++) {
+        for(int j=0;j<s;j++) {
+            if(i==0||i==(s-1)||j==0||j==(s-1))
+            	cout<<"*\t";
+            else
+            	cout<<"\t";
+        }
+        cout<<"\n";
+    }
+	return 0;
+}
