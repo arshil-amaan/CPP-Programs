ebd3d107cb4a72aedd447e379f802f233326b278
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-14 02:16:42 +0530

    Create  Right Angle Triangle using nested loops.cpp

---
+++ Right Angle Triangle using nested loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//Right Angle Triangle using nested loops
+int main()
+{
+    int s;
+    cin>>s;
+    //for rows
+    for(int i=0;i<s;i++)
+    {
+        //for columns
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*  ";
+        }
+        cout<<"\n";
+    }
+	return 0;
+}
