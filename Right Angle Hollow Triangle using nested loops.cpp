7cb82bc663a6fc74be9d0290b324e45fc2c68c66
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-14 02:17:18 +0530

    Create  Right Angle Hollow Triangle using nested loops.cpp

---
+++ Right Angle Hollow Triangle using nested loops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//Right Angle Hollow Triangle using nested loops
+int main()
+{
+    int s;
+    cin>>s;
+    for(int i=0;i<s;i++)
+    {
+        for(int j=0;j<s;j++)
+        {
+            if(j==0||j==i||i==s-1)
+            cout<<"*  ";
+            else
+            cout<<"   ";
+        }
+        cout<<"\n";
+    }
+	return 0;
+}
