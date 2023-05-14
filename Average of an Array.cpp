2506be9944746273ab5aa0cc3549b61c63ca44b3
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-15 00:43:58 +0530

    Create Average of an Array.cpp

---
+++ Average of an Array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n, sum=0;
+    double avg;
+    cout<<"Enter number of Elements"<<endl;
+    cin>>n;
+	int arr[n];
+	cout<<"Array has been Created"<<endl;
+	cout<<"Now enter Elements of Array"<<endl;
+	for (int i=0;i<n;i++) {
+	    cin>>arr[i];
+	}
+	cout<<"Entered Array is"<<endl;
+	for (int i=0;i<n;i++) {
+	    cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	for(int i=0;i<n;i++)
+	{
+	    sum+=arr[i];
+	}
+	avg=(double)sum/n;
+	cout<<"Average of Entered Array is "<<avg;
+	return 0;
+}
