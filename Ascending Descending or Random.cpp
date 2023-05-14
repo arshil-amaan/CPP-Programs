d92f2189e9fec33759c025770be6913d94acdd6c
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-15 00:41:23 +0530

    Create Ascending Descending or Random.cpp

---
+++ Ascending Descending or Random.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+//Returns true if Array Elements are Ascending
+bool ascCheck(int arr[],int n){
+    for(int i=1;i<n-1;i++){
+         if(arr[i-1]>=arr[i]){
+            return false;
+         }
+    }
+    return true;
+}
+//Returns true if Array Elements are Descending
+bool dscCheck(int arr[],int n){
+    for(int i=1;i<n-1;i++){
+         if(arr[i-1]<=arr[i]){
+            return false;
+         }
+    }
+    return true;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    //Values Inserted
+    for(int i=0;i<n;i++){
+        arr[i]=0;
+        cin>>arr[i];
+    }
+    if(ascCheck(arr,n))
+    cout<<"Ascending";
+    else if(dscCheck(arr,n))
+    cout<<"Descending";
+    else
+    cout<<"Neither Ascending nor Descending";
+	return 0;
+}
