768895996e9b50c61c78309909ad3b3de46603f2
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-16 01:17:30 +0530

    Create String Traversing Methods.cpp

---
+++ String Traversing Methods.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+ 
+int main() {
+	//array traversing methods
+	string mystr = "My String";
+ 
+	//Traverse Method 1
+	for(int i=0;i<mystr.length();i++)
+	cout<<mystr[i];
+	cout<<endl;
+ 
+	//Traverse Method 2
+	for(char x : mystr)
+	cout<<x;
+	cout<<endl;
+ 
+	//Traverse Method 3
+	for(auto i=mystr.begin();i!=mystr.end();i++)
+	cout<<(*i);
+	return 0;
+}
