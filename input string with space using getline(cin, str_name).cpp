c720e443ec34de534a81b059129b0f0f95975263
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-16 01:15:39 +0530

    Create input string with space using getline(cin, str_name).cpp

---
+++ input string with space using getline(cin, str_name).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string myString;
+    //cin>>str_name; will stop taking input if space encountered
+    // cin>>myString;
+    //so use getline(cin, str_name); instead
+    getline(cin, myString);
+    cout<<"Entered String is "<<myString;
+	return 0;
+}
