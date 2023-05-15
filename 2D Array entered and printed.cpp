d2710a39ab373409efc686c10b4163b00ae28f21
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-16 01:12:03 +0530

    Create 2D Array entered and printed.cpp

---
+++ 2D Array entered and printed.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int ar,ac;
+    cin>>ar>>ac;
+    int arr[ar][ac];
+    //entering elements in array
+    for(int i=0;i<ar;i++)
+    {
+        for(int j=0;j<ac;j++)
+        {
+            cin>>arr[i][j];
+        }    
+    }
+    //printing elements of array
+    for(int i=0;i<ar;i++)
+    {
+        for(int j=0;j<ac;j++)
+        {
+            cout<<arr[i][j]<<"  ";
+        }
+        cout<<"\n";
+    }
+	return 0;
+}
