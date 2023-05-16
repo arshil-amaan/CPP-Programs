71a78182531fcd4264df3e8b783a1c8a10769d85
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-17 02:11:52 +0530

    Create Find any substring in string and count.cpp

---
+++ Find any substring in string and count.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+ 
+int main() {
+    string name,findIt;
+    int count = 0;
+ 
+    cout<<"Enter Any Sentence"<<endl;
+    // name = "arshil amaan ansari"
+    getline(cin,name);
+ 
+    cout<<"string is \""<<name<<"\""<<endl;
+    cout<<"What you want to find or count?"<<endl;
+    cin>>findIt;
+ 
+    cout<<"\n";
+    int index = name.find(findIt);
+    while(index!=string::npos)
+    {
+        //programmers method
+        //cout<<"At index "<<index<<endl;
+        //index = name.find(findIt,index+1);
+ 
+        //better for user
+        cout<<"At index "<<++index<<endl;
+        index = name.find(findIt,index);
+ 
+        count++;
+	}
+	cout<<"Total "<<count<<" results found"<<endl;
+	return 0;
+}
