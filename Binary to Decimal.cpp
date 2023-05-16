98a1c68c2fefd1674f20fb5b0586c690d72d7f0c
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-17 02:09:52 +0530

    Create Binary to Decimal.cpp

---
+++ Binary to Decimal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+ 
+int main() {
+	int binary,decimal=0,len=0,temp,temp2,ld;
+	cin>>binary;
+	temp=binary;
+	temp2=binary;
+	while(temp)
+	{
+		temp=temp/10;
+		len++;
+	}
+ 
+	for(int i=0;i<=len;i++)
+	{
+		ld=temp2%10;
+		temp2=temp2/10;
+		decimal=decimal+pow(2,i)*ld;
+	}
+	cout<<"Decimal Equivalent of "<<binary<<" is "<<decimal<<endl;
+	return 0;
+}
