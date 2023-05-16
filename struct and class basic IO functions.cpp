88edf77bdc8b2b19de50ad6454add336bf3b9224
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-17 02:12:32 +0530

    Create struct and class basic IO functions.cpp

---
+++ struct and class basic IO functions.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+ 
+//only difference between class and struct is
+//by default struct are public and class are private
+//but we can make them public or private by below commented method
+ 
+ //created class student
+ //class student
+ //{  public:
+ //    //struct student has int type roll no string name int type rank
+ //    int rn;
+ //    string name;
+ //    int rank;
+ //};
+ 
+ 
+//created struct student
+ struct student
+ {
+ 	//struct student has int type roll no string name int type rank
+     int rn;
+     string name;
+     int rank;
+ };
+ 
+int main() {
+    //created student type variables
+    student abc;
+    student xyz;
+ 
+    //data storing in struct method 1
+    abc = {23,"Amar",15};
+ 
+    //data storing in struct method 
+    xyz.rn = 25;
+    xyz.name = "Akbar";
+ 
+    //print struct data
+    cout<<abc.rn<<" "<<abc.name<<" "<<abc.rank<<endl;
+ 
+    //by default garbage values will be stored in each
+    cout<<xyz.rn<<" "<<xyz.name<<" "<<xyz.rank<<endl;
+	return 0;
+}
