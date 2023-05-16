4e51923e87dfc21c7af74004ae9edeb6c2474a89
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-17 02:15:41 +0530

    Create execution time reduction.cpp

---
+++ execution time reduction.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+	//adding these two lines reduces considerable amount of execution time
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
