02bad455135621f124c22a13ace3ec331f0e4506
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-18 00:28:59 +0530

    Create Reverse words in a given string.cpp

---
+++ Reverse words in a given string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void reverseWords(string s)
+{
+ 
+  string revstr="",temp="";
+  for(int i=0;i<=s.length();i++)
+  {
+    if(s[i]==' ' || i==s.length())
+    {
+      revstr=temp+" "+revstr;
+      temp="";
+ 
+    }
+    else
+    {
+      temp=temp+s[i];
+    }
+  }
+  for(int i=0;i<revstr.length()-1;i++)
+  cout<<revstr[i];
+ 
+}
+int main() 
+{
+    string str = "i like this program very much";
+    reverseWords(str);
+    return 0;
+}
