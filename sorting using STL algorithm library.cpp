9769f52f409729e85e19618c7cdf260bfae29428
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-18 01:08:09 +0530

    Create sorting using STL algorithm library.cpp

---
+++ sorting using STL algorithm library.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+	//array initialized
+    int N;
+    cin>>N;
+    int arr[N];
+    for(int i=0;i<N;i++)
+        cin>>arr[i];
+ 
+   //array print
+    for(auto n : arr)
+		cout<<n<<" ";
+ 
+    //Sort function called
+	sort(arr,arr+5);
+ 
+	cout<<"\nAfter sort\n";
+ 
+	//array print
+    for(auto n : arr)
+        cout<<n<<" ";
+ 
+    //binary search function called
+    //(start,end,element)
+    if(binary_search(arr,arr+5,85))
+	cout<<"\nPresent";
+	else
+	cout<<"\nNot Present";
+	return 0;
+}
