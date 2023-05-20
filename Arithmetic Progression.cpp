3a271cd2f41ce9d7426ad6269cd269e9172fad19
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-21 00:49:24 +0530

    Update Arithmetic Progression.cpp

--- Arithmetic Progression.cpp
+++ Arithmetic Progression.cpp
@@ -5,12 +5,15 @@ using namespace std;
 int main() {
 	int A,C;
 	float B;
+	
 	cout<<"Enter A\n";
 	cin>>A;
 	cout<<"\nEnter B\n";
 	cin>>C;
+	
 	//storing mid point of A and C in B
 	B=(float)(A+C)/2;
+	
 	//checking if B is integer
 	int temp=B/1;
 	if(B==temp)
