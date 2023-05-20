b4aa97f14eaed927ea27e5d2c386d0000557993e
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-21 01:12:17 +0530

    Update Call by reference (*a) pointer as argument.cpp

--- Call by reference (*a) pointer as argument.cpp
+++ Call by reference (*a) pointer as argument.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
 using namespace std;
-void reverse_dig(int *n)
-{
-  int ans=0,ld;
-  while(*n)
-  {  
-    ld=*n%10;
-    *n=*n/10;
-    ans=ans*10+ld;
-  }
-  *n=ans;
+
+void reverse_dig(int *n) {
+    int ans=0,ld;
+    while(*n) {  
+        ld=*n%10;
+        *n=*n/10;
+        ans=ans*10+ld;
+    }
+    *n=ans;
 }
  
-void swap(int *a,int *b)
-{
-  int temp=*a;
-  *a=*b;
-  *b=temp;
+void swap(int *a,int *b) {
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }
- 
-int main() 
-{
+
+int main() {
     int a,b;
     cin>>a>>b;
     int *pa,*pb;
     pa=&a;
     pb=&b;
- 
+
     reverse_dig(pa);
     reverse_dig(pb);
- 
+
     swap (pa,pb);
- 
+
     cout<<"\n"<<a<<" "<<b;
- 
 }
