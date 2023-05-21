36d84500922743aedd393c790943a0c19c9b3840
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-22 02:43:32 +0530

    Update Distinct Element in Array.cpp

--- Distinct Element in Array.cpp
+++ Distinct Element in Array.cpp
@@ -2,14 +2,14 @@
 using namespace std;
  
 int main() {
-    int n, count=0, temp;
+	int n, count=0, temp;
     cout<<"Enter number of Elements"<<endl;
     cin>>n;
     temp=n;
 	int arr[n];
 	cout<<"Array has been Created"<<endl<<"Now enter Elements of Array"<<endl;
 	for (int i=0;i<n;i++) {
-	    cin>>arr[i];
+		cin>>arr[i];
 	}
 	cout<<"Entered Array is"<<endl;
 	for (int i=0;i<n;i++) {
@@ -19,9 +19,8 @@ int main() {
 	for (int i=0;i<n;i++) {
 	    // cout<<"checking for "<<arr[i]<<endl;
 	    for (int j=i+1;j<n;j++) {
-	        if(arr[i]==arr[j])
-	        {
-	            count++;
+	        if(arr[i]==arr[j]) {
+				count++;
 	            break;
 	        }
 	    }
