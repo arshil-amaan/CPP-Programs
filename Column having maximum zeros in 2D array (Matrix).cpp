3d05b839c490e9b391e9f248a3ab2281542ba838
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-22 02:42:16 +0530

    Update Column having maximum zeros in 2D array (Matrix).cpp

--- Column having maximum zeros in 2D array (Matrix).cpp
+++ Column having maximum zeros in 2D array (Matrix).cpp
@@ -2,40 +2,40 @@
 using namespace std;
  
 int main() {
-  int n,maxzer=0,zerincol=0,finans=0;
-  cin>>n;
-  int arr[n][n];
+    int n,maxzer=0,zerincol=0,finans=0;
+    cin>>n;
+    int arr[n][n];
  
-  for(int i=0;i<n;i++)
-    for(int j=0;j<n;j++) {
-      cin>>arr[i][j];
-    }
- 
-  //print arr
-  for(int i=0;i<n;i++) {
-    for(int j=0;j<n;j++) {
-      cout<<arr[i][j]<<"\t";
-    }
-    cout<<"\n";
-  }
- 
-  cout<<"\n";
- //counting zeros in each col
-  for(int j=0;j<n;j++) {
-    zerincol=0;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++) {
+            cin>>arr[i][j];
+        }
+    
+    //print arr
     for(int i=0;i<n;i++) {
-      if(arr[i][j]==0)
-      zerincol++;
+        for(int j=0;j<n;j++) {
+            cout<<arr[i][j]<<"\t";
+        }
+        cout<<"\n";
     }
-    cout<<"zeros in col "<<j<<" - "<<zerincol<<"\n";
-    // storing numbr of zeros if more than previous
-   if(zerincol>maxzer) {
-      maxzer=zerincol;
-      finans=j;
-    }
-  }
  
-  cout<<"\nmaximum "<<maxzer<<" zeros are there, which are in column index "<<finans;
+    cout<<"\n";
+    //counting zeros in each col
+    for(int j=0;j<n;j++) {
+        zerincol=0;
+        for(int i=0;i<n;i++) {
+            if(arr[i][j]==0)
+            zerincol++;
+        }
+        cout<<"zeros in col "<<j<<" - "<<zerincol<<"\n";
+        // storing numbr of zeros if more than previous
+        if(zerincol>maxzer) {
+            maxzer=zerincol;
+            finans=j;
+        }
+    }
+ 
+    cout<<"\nmaximum "<<maxzer<<" zeros are there, which are in column index "<<finans;
  
     return 0;
 }
