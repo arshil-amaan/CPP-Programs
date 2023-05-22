4ee36e5eb7a74e622884abb32ada6653499ecb7a
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-23 02:24:34 +0530

    Update Function and reference combined.cpp

--- Function and reference combined.cpp
+++ Function and reference combined.cpp
@@ -5,16 +5,16 @@ int &fun() {
     //non static variables don't exist once function call is over
     //hence never return reference, a non static local variable
 	static int x=10;
-    return x;
+	return x;
 }
 int main() {
-    //return value of fun() referenced with z
-    int &z=fun();
-    //fun() execute normally
-    cout<<fun()<<" ";
-    //changes made in z
-    z=25;
-    //hence changes occurred in fun()
-    cout<<fun();
+	//return value of fun() referenced with z
+	int &z=fun();
+	//fun() execute normally
+	cout<<fun()<<" ";
+	//changes made in z
+	z=25;
+	//hence changes occurred in fun()
+	cout<<fun();
 	return 0;
 }
