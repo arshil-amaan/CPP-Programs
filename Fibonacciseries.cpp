45fe8fdb4668068b5fd0214e178313212bc55654
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-23 02:21:41 +0530

    Update Fibonacciseries.cpp

--- Fibonacciseries.cpp
+++ Fibonacciseries.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace std;
 //fibonacci series number on place of 'num'
-int fibo(int n)
-{
+int fibo(int n) {
 	if(n==1 || n==2)
 		return 1;
-	else
-	{	int a=1,b=1, c;
-	    for(int i=3;i<=n;i++)
-	    {
-	        c=a+b;
+	else {
+		int a=1,b=1, c;
+	    for(int i=3;i<=n;i++) {
+			c=a+b;
 	        a=b;
 	        b=c;
 	        //un-comment below line for full fibonacci series
@@ -19,8 +17,7 @@ int fibo(int n)
     }
 }
  
-int main()
-{
+int main() {
     int num;
     cin>>num;
     // cout<<"1 1 ";
