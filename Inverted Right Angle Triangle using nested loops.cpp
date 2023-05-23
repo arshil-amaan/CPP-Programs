896432466b1d575a921d12f8fec0511473200085
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-24 01:22:02 +0530

    Update Inverted Right Angle Triangle using nested loops.cpp

--- Inverted Right Angle Triangle using nested loops.cpp
+++ Inverted Right Angle Triangle using nested loops.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 using namespace std;
 //Inverted Right Angle Triangle using nested loops
-int main()
-{
-    int s;
-    cin>>s;
-    for(int i=0;i<s;i++)
-    {
-        for(int j=0;j<(s-i);j++)
-        {
-           cout<<"*  ";
-        }
-        cout<<"\n";
-    }
+int main() {
+	int s;
+	cin>>s;
+	for(int i=0;i<s;i++) {
+		for(int j=0;j<(s-i);j++)
+			cout<<"*  ";
+
+		cout<<"\n";
+	}
 	return 0;
 }
