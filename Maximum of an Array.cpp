cb231cd037159f9b257715c8fddd8b823895e871
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-24 01:32:55 +0530

    Update Maximum of an Array.cpp

--- Maximum of an Array.cpp
+++ Maximum of an Array.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
- 
+
 int main() {
-    int n, max;
-    cout<<"Enter number of Elements"<<endl;
-    cin>>n;
+	int n, max;
+	cout<<"Enter number of Elements"<<endl;
+	cin>>n;
 	int arr[n];
 	cout<<"Array has been Created"<<endl;
 	cout<<"Now enter Elements of Array"<<endl;
-	for (int i=0;i<n;i++) {
-	    cin>>arr[i];
-	}
+	for (int i=0;i<n;i++)
+		cin>>arr[i];
+	
 	max=arr[0];
 	cout<<"Entered Array is"<<endl;
 	for (int i=0;i<n;i++) {
