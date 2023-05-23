0f3f87714b7e9496096c220481cb2618f177a36d
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-24 01:27:05 +0530

    Update Linked List with 3 (manual) nodes.cpp

--- Linked List with 3 (manual) nodes.cpp
+++ Linked List with 3 (manual) nodes.cpp
@@ -1,46 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct node
-{
-  int data;
-  node *next;
+struct node {
+    int data;
+    node *next;
 };
  
 //linked list print
-void printlinkdlist(node *n)
-{
-  while(n!=NULL)
-  {
-    cout<<n->data<<" ";
-    n=n->next;
-  }
+void printlinkdlist(node *n) {
+    while(n!=NULL) {
+        cout<<n->data<<" ";
+        n=n->next;
+    }
 }
-int main() 
-{
-  //creating three nodes with no value
-  //created 3 instance
-  node *head =NULL;
-  node *second =NULL;
-  node *third =NULL;
+int main() {
+    //creating three nodes with no value
+    //created 3 instance
+    node *head =NULL;
+    node *second =NULL;
+    node *third =NULL;
  
-  //allocating 3 nodes in the heap
-  head = new node();
-  second = new node();
-  third = new node();
+    //allocating 3 nodes in the heap
+    head = new node();
+    second = new node();
+    third = new node();
  
-  head->data = 10;
-  head->next = second;
- 
- 
-  second->data = 20;
-  second->next = third;
- 
- 
-  third->data = 30;
-  third->next = NULL;
- 
-  //calling function to print elements
-  printlinkdlist(head);
- 
-  return 0;
+    head->data = 10;
+    head->next = second;
+
+
+    second->data = 20;
+    second->next = third;
+
+
+    third->data = 30;
+    third->next = NULL;
+
+    //calling function to print elements
+    printlinkdlist(head);
+
+    return 0;
 }
