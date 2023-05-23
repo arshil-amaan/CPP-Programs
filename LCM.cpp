c1bb3731156cc854fd13c9a729fdf41e47d72ee9
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-24 01:23:12 +0530

    Update LCM.cpp

--- LCM.cpp
+++ LCM.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 using namespace std;
 //Least Common Multiple
-int lcm(int a, int b)
-{
-    for(int i=max(a,b);i<a*b;i++)
-    if(i%a==0 && i%b==0)
-    {
-        return i;
-    }
+int lcm(int a, int b) {
+	for(int i=max(a,b);i<a*b;i++)
+	if(i%a==0 && i%b==0)
+		return i;
 }
-int main()
-{
-    int a,b;
-    cin>>a>>b;
-    cout<<lcm(a,b);
+int main() {
+	int a,b;
+	cin>>a>>b;
+	cout<<lcm(a,b);
 	return 0;
 }
