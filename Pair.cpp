7d12d1db9c966536ea28a2f5cd323e442c96d06c
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-26 01:12:26 +0530

    Update Pair.cpp

--- Pair.cpp
+++ Pair.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main() {
 	//pair has strictly two things in it 
 	pair<int, string> p1(100,"Arshil");
- 
+
 	//default assigned values are 0 for int, " "(space) for string
 	pair<int, int> p2;
- 
+
 	//can just be declared and initialized later
 	pair<string, int> p3;
 	p3={"name", 80};
- 
+
 	//accessed pair members using .first .second
 	cout<<p1.first<<" "<<p1.second<<"\n";
 	cout<<p2.first<<" "<<p2.second<<"\n";
