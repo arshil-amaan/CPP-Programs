2dd6ee585f45ab42228de3cc71226a3d768914ba
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-26 01:09:44 +0530

    Update Number Reversed.cpp

--- Number Reversed.cpp
+++ Number Reversed.cpp
@@ -1,20 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int t,N;
-    cin>>t;
-    while(t--)
-    {
-      int rev=0;
-      cin>>N;
-      while(N)
-      {
-	      //storing last digit at ones place in each loop
-	      rev=rev*10+N%10;
-	      //removing last digit
-	      N=N/10;
-      }
-      cout<<rev<<endl;
-    }
-    return 0;
+	int t,N;
+	cin>>t;
+	while(t--) {
+		int rev=0;
+		cin>>N;
+		while(N) {
+			//storing last digit at ones place in each loop
+			rev=rev*10+N%10;
+			//removing last digit
+			N=N/10;
+		}
+		cout<<rev<<endl;
+	}
+  return 0;
 }
