e368aa26d10fc6bf399d0389530889e72d09459a
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-26 01:14:05 +0530

    Update Palindrome check.cpp

--- Palindrome check.cpp
+++ Palindrome check.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 using namespace std;
-
 int main() {
   string n;
   cin>>n;
-  for(int i=0;i<n.length()/2;i++)
-    {
-        if(n[i]!=n[n.length()-i-1])
-        {
-            cout<<"Not Palindrome"<<endl;
-    	    return 0;
-        }
+  for(int i=0;i<n.length()/2;i++) {
+    if(n[i]!=n[n.length()-i-1]) {
+      cout<<"Not Palindrome"<<endl;
+      return 0;
     }
-    cout<<"Palindrome";
-    return 0;
+  }
+  cout<<"Palindrome";
+  return 0;
 }
