3d83f8e56320e256504d5ca0f9e0fa269cea862d
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-27 00:29:04 +0530

    Update Prime, Composite, Divisors.cpp

--- Prime, Composite, Divisors.cpp
+++ Prime, Composite, Divisors.cpp
@@ -15,20 +15,21 @@ bool is_prime(int a) {
  
 void divisors(int a) {
     for(int i=2;i<a;i++) {
-    	//checking if any number able to fully divide given number
+        //checking if any number able to fully divide given number
         if(a%i==0)
-        //printing the numbers which can fully divide given number
-        cout<<i<<" ";
+            //printing the numbers which can fully divide given number
+            cout<<i<<" ";
     }
 }
+
 int main() {
     int b;
     cin>>b;
  
     if(is_prime(b))
-    	cout<<b<<" is Prime";
+        cout<<b<<" is Prime";
     else
-    	cout<<"Below are its all divisors \n";
+        cout<<"Below are its all divisors \n";
  
     divisors(b);
  
