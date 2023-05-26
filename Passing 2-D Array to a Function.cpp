20511833299261e4a385728812a11558e648b710
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-27 00:22:02 +0530

    Update Passing 2-D Array to a Function.cpp

--- Passing 2-D Array to a Function.cpp
+++ Passing 2-D Array to a Function.cpp
@@ -1,41 +1,34 @@
 #include<iostream>
 using namespace std;   
- 
-void show(int (*q)[3], int  row, int  col)
-{
-	int  i, j ;
+
+void show(int (*q)[3], int  row, int  col) {
+	int i,j;
 	for(i=0;i<row;i++)
-	{
 		for(j=0;j<col;j++)
-    		cout<<"\t"<<*(*(q + i)+j); 
+			cout<<"\t"<<*(*(q + i)+j); 
 		cout<<"\n";
-	}  
     cout<<"\n";
-} 
- 
-void print(int  q[][4], int row, int col)
-{
-    int  i, j; 
-    for(i=0;i<row;i++)
-    {   
-	 for(j=0;j<col;j++)
-   	    	cout<<"\t"<<q[i][j];
-   	 cout<<"\n";
-	}
-   cout<<"\n";
 }
  
-int main() 
-{
-  int  a[3][4] = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21} ; 
- 
-  int row, col;
-  cin>>row>>col;
-  int  arr[row][3];
- 
-  for(int i=0; i<row;i++)
-    for(int j=0; j<col;j++)
-      cin>>arr[i][j];
+void print(int  q[][4], int row, int col) {
+	int i,j; 
+	for(i=0;i<row;i++)
+		for(j=0;j<col;j++)
+			cout<<"\t"<<q[i][j];
+		cout<<"\n";
+	cout<<"\n";
+}
+
+int main() {
+	int  a[3][4] = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21} ; 
+
+	int row, col;
+	cin>>row>>col;
+	int  arr[row][3];
+
+	for(int i=0; i<row;i++)
+		for(int j=0; j<col;j++)
+			cin>>arr[i][j];
  
  
   show (arr, row, col);
