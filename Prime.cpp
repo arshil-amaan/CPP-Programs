b59b323e33f37ed3b602e59c30802cc1cc603e8e
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-27 00:29:57 +0530

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -3,16 +3,14 @@ using namespace std;
 //Prime
 int main()
 {
-    int n;
-    cin>>n;
-    for(int i=2;i<n;i++)
-    {
-        if(n%i==0)
-        {
-            cout<<n<<" is NON Prime.";
-            return 0;
-        }
-    }
-    cout<<n<<" is Prime.";
+	int n;
+	cin>>n;
+	for(int i=2;i<n;i++) {
+		if(n%i==0) {
+			cout<<n<<" is NON Prime.";
+			return 0;
+		}
+	}
+	cout<<n<<" is Prime.";
 	return 0;
 }
