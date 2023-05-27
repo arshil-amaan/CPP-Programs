dcde377bd480c22eaa96ca68f5c6d18b90a850f2
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-28 01:36:41 +0530

    Update Right Angle Triangle using nested loops.cpp

--- Right Angle Triangle using nested loops.cpp
+++ Right Angle Triangle using nested loops.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 //Right Angle Triangle using nested loops
 int main() {
-    int s;
-    cin>>s;
-    //for rows
-    for(int i=0;i<s;i++)
-    {
-        //for columns
-        for(int j=0;j<=i;j++)
-        {
-            cout<<"*  ";
-        }
-        cout<<"\n";
+	int s;
+	cin>>s;
+	//for rows
+	for(int i=0;i<s;i++) {
+		//for columns
+		for(int j=0;j<=i;j++) {
+			cout<<"*  ";
+		}
+		cout<<"\n";
     }
 	return 0;
 }
