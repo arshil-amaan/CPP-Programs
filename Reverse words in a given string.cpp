6c69cd68bec7b5e4b30830c8f66124e9d88e9876
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-28 01:30:51 +0530

    Update Reverse words in a given string.cpp

--- Reverse words in a given string.cpp
+++ Reverse words in a given string.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 void reverseWords(string s) {
- 
-  string revstr="",temp="";
-  for(int i=0;i<=s.length();i++) {
-    if(s[i]==' ' || i==s.length()) {
-      revstr=temp+" "+revstr;
-      temp="";
- 
-    } else {
-      temp=temp+s[i];
+
+    string revstr="",temp="";
+    for(int i=0;i<=s.length();i++) {
+        if(s[i]==' ' || i==s.length()) {
+            revstr=temp+" "+revstr;
+            temp="";
+        } else {
+            temp=temp+s[i];
+        }
     }
-  }
-  for(int i=0;i<revstr.length()-1;i++)
-  cout<<revstr[i];
- 
+    for(int i=0;i<revstr.length()-1;i++)
+        cout<<revstr[i];
 }
 int main() 
 {
