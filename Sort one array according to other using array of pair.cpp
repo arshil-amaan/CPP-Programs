917c10f39314ceb59b0d7d804646c8ff1c0e0e3e
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-29 02:02:47 +0530

    Update Sort one array according to other using array of pair.cpp

--- Sort one array according to other using array of pair.cpp
+++ Sort one array according to other using array of pair.cpp
@@ -4,21 +4,17 @@ using namespace std;
 void sortChar(int intarr[],char chararr[],int n) {
 	// array of pair
 	pair<int,char> pairarr[n];
- 
+
 	// assigning elements to array of pair
 	for(int i=0;i<n;i++)
-	{
 		pairarr[i]={intarr[i],chararr[i]};
-	}
  
 	// sorting is performed over first element by default
 	sort(pairarr,pairarr + n);
  
 	// Printing second elements of array of pair
 	for(int i=0;i<n;i++)
-	{
 		cout<<pairarr[i].second<<" ";
-	}
 }
  
 int main() {
