a8f3b44d5054d9ae3984c6a4b68896cf1334d1e4
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-29 02:11:08 +0530

    Update Square Wall single Loop and string Multiplier.cpp

--- Square Wall single Loop and string Multiplier.cpp
+++ Square Wall single Loop and string Multiplier.cpp
@@ -7,16 +7,14 @@ string operator * (string a, unsigned int b) {
 		output += a;
 	return output;
 }
-int main()
-{
-    int s;
-    string st="";
-    cin>>s;
-    for(int i=0;i<s;i++)
-    {
-        st=st+"*  ";
-    }
-    st=st+"\n";
-    cout<<st*s;
+int main() {
+	int s;
+	string st="";
+	cin>>s;
+	for(int i=0;i<s;i++)
+		st=st+"*  ";
+	
+	st=st+"\n";
+	cout<<st*s;
 	return 0;
 }
