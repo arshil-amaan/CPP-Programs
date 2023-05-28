232cf0ce2078c1dbcaa29f237e8f8ee7d5aaaf86
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-29 02:12:46 +0530

    Update Square wall using nested loops.cpp

--- Square wall using nested loops.cpp
+++ Square wall using nested loops.cpp
@@ -2,17 +2,14 @@
 using namespace std;
 //square wall using nested loops
 int main() {
-    int s;
-    cin>>s;
-    //for rows
-    for(int i=0;i<s;i++)
-    {
-        //for columns
-        for(int j=0;j<s;j++)
-        {
-            cout<<"*  ";
-        }
-        cout<<"\n";
-    }
+	int s;
+	cin>>s;
+	//for rows
+	for(int i=0;i<s;i++) {
+		//for columns
+		for(int j=0;j<s;j++)
+			cout<<"*  ";
+		cout<<"\n";
+	}
 	return 0;
 }
