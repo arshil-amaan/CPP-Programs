2b13d052792cf2d417c5e09e80637f701d91e275
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-31 00:18:49 +0530

    Update access and modify struct member -> operator.cpp

--- access and modify struct member -> operator.cpp
+++ access and modify struct member -> operator.cpp
@@ -6,27 +6,27 @@ struct point{
   int y;
 };
 int main() {
-    //two different methods for data input in struct elements
-    point p = {2,3};
-    point q{5,9};
- 
-    //creating two pointer variables for two point type variables
-    point *ptr = &p;
-    point *ptr2 = &q;
- 
-    //when we access struct member using pointer we use  " -> "  operator
-    //ptr->y will access data at y of p only
-    //because p is assigned to *ptr
-    cout<<ptr->y<<endl;
-    cout<<ptr2->y<<endl;
- 
-    //changing data using -> operator
-    ptr->y = 7;
- 
-    //changing data at p.x
-    p.x=6;
- 
-    //printing updated value using both methods
-    cout<<p.x<<" "<<ptr->y<<endl;
-    return 0;
+  //two different methods for data input in struct elements
+  point p = {2,3};
+  point q{5,9};
+
+  //creating two pointer variables for two point type variables
+  point *ptr = &p;
+  point *ptr2 = &q;
+
+  //when we access struct member using pointer we use  " -> "  operator
+  //ptr->y will access data at y of p only
+  //because p is assigned to *ptr
+  cout<<ptr->y<<endl;
+  cout<<ptr2->y<<endl;
+
+  //changing data using -> operator
+  ptr->y = 7;
+
+  //changing data at p.x
+  p.x=6;
+
+  //printing updated value using both methods
+  cout<<p.x<<" "<<ptr->y<<endl;
+  return 0;
 }
