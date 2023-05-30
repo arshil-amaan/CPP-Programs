84eb821bea4609f96817bb399cb8321248c6d9bb
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-30 14:41:40 +0530

    Update Swap using References.cpp

--- Swap using References.cpp
+++ Swap using References.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 void swap(int &x, int &y){
-    int temp = x;
-    x=y;
-    y=temp;
+	int temp = x;
+	x=y;
+	y=temp;
 }
 int main() {
-    int a,b;
-    cout<<"A = "<<endl;
-    cin>>a;
-    cout<<"\nB = "<<endl;
-    cin>>b;
-    cout<<"Before Swap";
-    cout<<"\n"<<"A = "<<a<<"\n"<<"B = "<<b<<endl;
-    swap(a,b);
-    cout<<"After Swap";
-    cout<<"\n"<<"A = "<<a<<"\n"<<"B = "<<b;
+	int a,b;
+	cout<<"A = "<<endl;
+	cin>>a;
+	cout<<"\nB = "<<endl;
+	cin>>b;
+	cout<<"Before Swap";
+	cout<<"\n"<<"A = "<<a<<"\n"<<"B = "<<b<<endl;
+	swap(a,b);
+	cout<<"After Swap";
+	cout<<"\n"<<"A = "<<a<<"\n"<<"B = "<<b;
 	return 0;
 }
