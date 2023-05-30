49c206ddb697ab5bda1a2336bf64c54fe9347d83
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-30 14:43:04 +0530

    Update Transpose of Matrix.cpp

--- Transpose of Matrix.cpp
+++ Transpose of Matrix.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 using namespace std;
- 
+
 int main() {
-    int row=3, col=3;
-    // cin>>row;
-    // cin>>col;
-    int arr[row][col];
-    //array input
-    for(int i=0;i<row;i++)
-    {
-        for(int j=0;j<col;j++)
-        {
-            cin>>arr[i][j];
-        }    
-    }
-    //printing entered array
-    for(int i=0;i<row;i++)
-    {
-        for(int j=0;j<row;j++)
-        {
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
+	int row=3, col=3;
+	// cin>>row;
+	// cin>>col;
+	int arr[row][col];
+	//array input
+	for(int i=0;i<row;i++) {
+		for(int j=0;j<col;j++)
+			cin>>arr[i][j];
+	//printing entered array
+	for(int i=0;i<row;i++) {
+		for(int j=0;j<row;j++)
+			cout<<arr[i][j]<<" ";
+		cout<<"\n";
+	}
     //anpther array created
     int brr[row][col];
     for(int i=0;i<row;i++)
