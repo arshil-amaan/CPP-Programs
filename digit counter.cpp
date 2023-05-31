40ffc75b42a950217425ed1964aa02ebbead9f66
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-31 19:04:29 +0530

    Update digit counter.cpp

--- digit counter.cpp
+++ digit counter.cpp
@@ -3,15 +3,14 @@ using namespace std;
 //how many digits in user provided integer
 int main()
 {
-    int num, digits=0;
-    cin>>num;
-    int n=num;
-    //Divide number by 10 and increase counter
-    while(n>0)
-    {
-        n=n/10;
-        digits++;
+	int num, digits=0;
+	cin>>num;
+	int n=num;
+	//Divide number by 10 and increase counter
+	while(n>0) {
+		n=n/10;
+		digits++;
     }
-    cout<<num<<" has "<<digits<<" Digits";
+	cout<<num<<" has "<<digits<<" Digits";
 	return 0;
 }
