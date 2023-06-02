4840030f309b130379406802d6a1898c2edd2250
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-03 01:55:00 +0530

    Update digital root.cpp

--- digital root.cpp
+++ digital root.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
- 
-int sumOfAll(int a)
-{
-    int ld, sum=0;
-    while(a>0){
-        ld=a%10;
-        a=a/10;
-        sum=sum+ld;
+
+int sumOfAll(int a) {
+	int ld, sum=0;
+	while(a>0){
+		ld=a%10;
+		a=a/10;
+		sum=sum+ld;
     }
     return sum;
 }
