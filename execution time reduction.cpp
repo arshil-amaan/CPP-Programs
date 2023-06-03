b63fe79e88b3b1e7cd1fa8613c561f9857ecaac8
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-04 01:17:33 +0530

    Update execution time reduction.cpp

--- execution time reduction.cpp
+++ execution time reduction.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	//adding these two lines reduces considerable amount of execution time
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	return 0;
 }
