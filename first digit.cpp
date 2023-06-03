00181545e23da64731ff5a8f52077cdf7b1a040b
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-04 01:19:39 +0530

    Update first digit.cpp

--- first digit.cpp
+++ first digit.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 //First Digit of given Number
 int firstDigit(int num) {
-    int fd =abs(num);
-    //removes ones digit untill it is single digit
-    while(fd>9)
-    {
-        fd=fd/10;
-    }
+	int fd =abs(num);
+	//removes ones digit untill it is single digit
+	while(fd>9) {
+		fd=fd/10;
+	}
 	return fd;
-}
+	}
 int main()
 {
 	int num;
