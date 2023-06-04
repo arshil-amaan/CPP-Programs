a4aeb53c7ed7c21399faccd5e701cdadc1f648fe
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-05 00:12:42 +0530

    Update odd even game.cpp

--- odd even game.cpp
+++ odd even game.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
 //Odd Even Game
-void oddEvenGame(int n)
-{
-    cout<<n<<endl;
-    //if even ==> Friend win
+void oddEvenGame(int n) {
+	cout<<n<<endl;
+	//if even ==> Friend win
 	if(n%2==0)
-	    cout<<"You are Winner of this round";
+		cout<<"You are Winner of this round";
 	else
-	//if odd ==> Ash win
-	    cout<<"Ash Wins this round";
+		//if odd ==> Ash win
+		cout<<"Ash Wins this round";
 }
 int main()
 {
