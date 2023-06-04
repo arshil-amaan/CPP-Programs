26a2a990119988533f13af85d634e0d62e7231bc
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-05 00:09:29 +0530

    Update isPrime.cpp

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 using namespace std;
 //Prime check
-bool isPrime(int n)
-{
-    if(n==1)
-    return false;
-    else
-    {
-        for(int i=2;i<n;i++)
-        {
-            if(n%i==0)
-            return false;
-        }
-        return true;
-    }
+bool isPrime(int n) {
+	if(n==1)
+		return false;
+	else {
+		for(int i=2;i<n;i++) {
+			if(n%i==0)
+			return false;
+		}
+		return true;
+	}
 }
 int main()
 {
