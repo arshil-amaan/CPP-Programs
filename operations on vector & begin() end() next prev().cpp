8c2268937578ba4cedb9d6240032a5cecf1da8fd
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-06 01:26:39 +0530

    Update operations on vector & begin() end() next prev().cpp

--- operations on vector & begin() end() next prev().cpp
+++ operations on vector & begin() end() next prev().cpp
@@ -16,26 +16,26 @@ int main() {
 	for(auto i=vect.begin();i<vect.end();i++)
 		cout<<(*i)<<" ";
  
-    cout<<"\n";
-    // target only first element of vector using begin()
-    vector<int>::iterator j=vect.begin();
-    cout<<*j<<" ";
-    // j++;
-    j=next(j);
-    cout<<*j<<" ";
+	cout<<"\n";
+	// target only first element of vector using begin()
+	vector<int>::iterator j=vect.begin();
+	cout<<*j<<" ";
+	// j++;
+	j=next(j);
+	cout<<*j<<" ";
  
-    // target only last element of vector using end()
-    // auto k can replace "vector<int>::iterator k"
-    auto k=--vect.end();
-    // k--;
-    cout<<*k<<" ";
-    // next() and prev() can take two arguments first address second index default index is 1
-    k=prev(k,2);
-    cout<<*k<<" ";
- 
-    // calculating and printing number of elements in vector
-    // vector elements can be accessed like array using index
-    cout<<"\nNumber of elements in vector vect = ";
-    cout<<sizeof(vect)/sizeof(vect[0])<<" ";
+	// target only last element of vector using end()
+	// auto k can replace "vector<int>::iterator k"
+	auto k=--vect.end();
+	// k--;
+	cout<<*k<<" ";
+	// next() and prev() can take two arguments first address second index default index is 1
+	k=prev(k,2);
+	cout<<*k<<" ";
+
+	// calculating and printing number of elements in vector
+	// vector elements can be accessed like array using index
+	cout<<"\nNumber of elements in vector vect = ";
+	cout<<sizeof(vect)/sizeof(vect[0])<<" ";
 	return 0;
 }
