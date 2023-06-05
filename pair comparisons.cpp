69c3324bbe22373d322cfc79f9612980cd03d35f
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-06 01:27:09 +0530

    Update pair comparisons.cpp

--- pair comparisons.cpp
+++ pair comparisons.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
- 
+
 int main() {
 	pair<int, int>p1(12,25), p2(12,10);
- 
+
 	//Only first elements are compared if they match then second are compared
 	cout<<(p1==p2)<<" "<<(p1!=p2)<<" "<<(p1>p2)<<" "<<(p1<p2);
 	return 0;
