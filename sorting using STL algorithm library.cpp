f76904a883de5b3f78efbf35438625a6b58487bf
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-06 01:31:08 +0530

    Update sorting using STL algorithm library.cpp

--- sorting using STL algorithm library.cpp
+++ sorting using STL algorithm library.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
- 
+
 int main() {
 	//array initialized
-    int N;
-    cin>>N;
-    int arr[N];
-    for(int i=0;i<N;i++)
-        cin>>arr[i];
- 
-   //array print
-    for(auto n : arr)
-		cout<<n<<" ";
- 
-    //Sort function called
-	sort(arr,arr+5);
- 
-	cout<<"\nAfter sort\n";
+	int N;
+	cin>>N;
+	int arr[N];
+	for(int i=0;i<N;i++)
+		cin>>arr[i];
  
 	//array print
-    for(auto n : arr)
-        cout<<n<<" ";
+	for(auto n : arr)
+		cout<<n<<" ";
  
-    //binary search function called
-    //(start,end,element)
-    if(binary_search(arr,arr+5,85))
+	//Sort function called
+	sort(arr,arr+5);
+
+	cout<<"\nAfter sort\n";
+
+	//array print
+	for(auto n : arr)
+		cout<<n<<" ";
+
+	//binary search function called
+	//(start,end,element)
+	if(binary_search(arr,arr+5,85))
 	cout<<"\nPresent";
 	else
 	cout<<"\nNot Present";
