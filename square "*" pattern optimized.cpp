465d28f42dcc384e0545334ad373321eba5a904f
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-06 01:32:37 +0530

    Update square "*" pattern optimized.cpp

--- square "*" pattern optimized.cpp
+++ square "*" pattern optimized.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 //how many digits in user provided integer
 int main() {
-    int s;
-    cin>>s;
-    for(int i=0;i<s;i++) {
-        for(int j=0;j<s;j++) {
-            if(i==0||i==(s-1)||j==0||j==(s-1))
-            	cout<<"*\t";
-            else
-            	cout<<"\t";
-        }
-        cout<<"\n";
-    }
+	int s;
+	cin>>s;
+	for(int i=0;i<s;i++) {
+		for(int j=0;j<s;j++) {
+			if(i==0||i==(s-1)||j==0||j==(s-1))
+				cout<<"*\t";
+			else
+				cout<<"\t";
+		}
+		cout<<"\n";
+	}
 	return 0;
 }
