352c670a3fbae7f373a0d915a11c7d629c9c4d01
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-06 01:28:27 +0530

    Update passing methods.cpp

--- passing methods.cpp
+++ passing methods.cpp
@@ -7,20 +7,20 @@ void fun(int x, int y) {
 }
 
 // pass by reference
-void fun(int &x, int &y){
+void fun(int &x, int &y) {
 	cout<<"x value : "<<x<<endl<<"y value : "<<y<<endl;
 }
 
 // pass by address
-void fun(int *x, int *y){
+void fun(int *x, int *y) {
 	cout<<"x is : "<<*x<<endl<<"y is : "<<*y<<endl;
 }
 
 int main() {
-    int a=10,b=15;
-  // pass by value, pass by reference
-    fun(a,b);
-  // pass by address
-    fun(&a,&b);
+	int a=10,b=15;
+	// pass by value, pass by reference
+	fun(a,b);
+	// pass by address
+	fun(&a,&b);
 	return 0;
 }
