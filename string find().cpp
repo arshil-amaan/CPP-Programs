15d0ebc167170eb42cd36699b6b19703156546a5
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-07 00:21:05 +0530

    Update string find().cpp

--- string find().cpp
+++ string find().cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 int main() {
 	//string created
-    string myString = "Myself Arshil Amaan Ansari";
- 
-    //previewing saved string
-    cout<<"Saved string is \""<<myString<<"\""<<endl;
- 
-    //asking what needs to be found
-    string findIt;
-    cout<<"What you wanna find?\n";
-    cin>>findIt;
- 
-    //storing result in res after find()
-    int res = myString.find(findIt);
- 
-    //if searched string not found find() returns "string::npos"
-    if(res == string::npos)
-    cout<<"Not found";
-    else
-    cout<<"First occurrence at "<<res;
+	string myString = "Myself Arshil Amaan Ansari";
+
+	//previewing saved string
+	cout<<"Saved string is \""<<myString<<"\""<<endl;
+
+	//asking what needs to be found
+	string findIt;
+	cout<<"What you wanna find?\n";
+	cin>>findIt;
+
+	//storing result in res after find()
+	int res = myString.find(findIt);
+
+	//if searched string not found find() returns "string::npos"
+	if(res == string::npos)
+		cout<<"Not found";
+	else
+		cout<<"First occurrence at "<<res;
 	return 0;
 }
