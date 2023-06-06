b88b4be16fbd2c000194ba3b603be8120b584648
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-07 00:20:11 +0530

    Update square "*" pattern.cpp

--- square "*" pattern.cpp
+++ square "*" pattern.cpp
@@ -2,26 +2,24 @@
 using namespace std;
 //square
 int main() {
-    int s;
-    cin>>s;
-    for(int i=1;i<=s;i++) {
-        //first and last row
-       if(i==1 || i==s) {
-            for(int j=1;j<=s;j++)
-            	cout<<"*\t";
-        }
-        else {
-            //middle rows
-            for(int j=1;j<=s;j++) {
-                if(j==1 || j==s)
-                	cout<<"*\t";
- 
-                //center part
-                else
-                cout<<"\t";
-            }
-        }
-        cout<<"\n";
-    }
+	int s;
+	cin>>s;
+	for(int i=1;i<=s;i++) {
+		//first and last row
+		if(i==1 || i==s) {
+			for(int j=1;j<=s;j++)
+				cout<<"*\t";
+		} else {
+			//middle rows
+			for(int j=1;j<=s;j++) {
+				if(j==1 || j==s)
+					cout<<"*\t";
+				//center part
+				else
+					cout<<"\t";
+			}
+		}
+		cout<<"\n";
+	}
 	return 0;
 }
