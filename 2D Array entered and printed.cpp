f366c6455865f2be52a838f1c91a9e23ae9ce9cf
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-16 02:30:34 +0530

    Update 2D Array entered and printed.cpp

--- 2D Array entered and printed.cpp
+++ 2D Array entered and printed.cpp
@@ -9,16 +9,16 @@ int main() {
 	
 	//entering elements in array
 	for(int i=0;i<row;i++) {
-        for(int j=0;j<col;j++) {
-            cin>>arr[i][j];
-        }    
-    }
-    //printing elements of array
-    for(int i=0;i<row;i++) {
-        for(int j=0;j<col;j++) {
-            cout<<arr[i][j]<<"  ";
-        }
-        cout<<"\n";
+		for(int j=0;j<col;j++) {
+			cin>>arr[i][j];
+		}
+	}
+	//printing elements of array
+	for(int i=0;i<row;i++) {
+		for(int j=0;j<col;j++) {
+			cout<<arr[i][j]<<"  ";
+		}
+		cout<<"\n";
     }
 	return 0;
 }
